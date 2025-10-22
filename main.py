481e100179df4fc95200c48408c0ad73a0dab07c
adyabala <adyab3211@gmail.com>
2025-10-22 10:15:11 +0530

    updating css

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import login_user,logout_user,login_manager,LoginManager
 from flask_login import login_required,current_user
-# from flask_mail import Mail
+
 
 
 
